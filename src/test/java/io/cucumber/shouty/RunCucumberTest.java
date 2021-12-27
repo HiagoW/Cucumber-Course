6ed19d918d2c61ae044328a3a3d877eea1f38f2d
Hiago Petris <hiago.petris@ibm.com>
2021-12-27 11:32:25 -0300

    New features and plugins

--- src/test/java/io/cucumber/shouty/RunCucumberTest.java
+++ src/test/java/io/cucumber/shouty/RunCucumberTest.java
@@ -5,14 +5,16 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("io/cucumber/shouty")
+// Disable pretty to generate html report, was not working when I runned it
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 // Need to set correct package to work
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.shouty")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value="@focus")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value="not @slow")
 public class RunCucumberTest {
 }
--- src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -1,28 +1,18 @@
 package io.cucumber.shouty;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
+import io.cucumber.java.DataTableType;
+import io.cucumber.java.Transpose;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import java.util.HashMap;
+import java.util.*;
 
-import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinitions {
-
-    private Person sean;
-    private Person lucy;
-    private String messageFromSean;
-    private Network network;
-    private HashMap<String, Person> people;
-
-    @Before
-    public void createNetwork() {
-        network = new Network();
-        people = new HashMap<>();
-    }
 
     // Example of custom parameter
     // / -> One or other word
@@ -33,19 +23,88 @@ public class StepDefinitions {
 //    @Given("{person} is located/standing {int} meter(s) from {person}")
 //    public void lucy_is_meters_from_sean(Person lucy, Integer distance, Person sean) {}
 
+    private static final int DEFAULT_RANGE = 100;
+    private String messageFromSean;
+    private Network network = new Network(DEFAULT_RANGE);
+    private Map<String, Person> people;
+
+    static class Whereabouts {
+        public String name;
+        public Integer location;
+
+        public Whereabouts(String name, int location) {
+            this.name = name;
+            this.location = location;
+        }
+    }
+
+    @DataTableType
+    public Whereabouts defineWhereabouts(Map<String, String> entry) {
+        return new Whereabouts(entry.get("name"), Integer.parseInt(entry.get("location")));
+    }
+
+    @Before
+    public void createNetwork() {
+        people = new HashMap<String, Person>();
+    }
+
+    @Given("the range is {int}")
+    public void the_range_is(int range) throws Throwable {
+        network = new Network(range);
+    }
+
     @Given("a person named {word}")
-    public void a_person_named(String name) {
-        people.put(name, new Person(network));
+    public void a_person_named(String name) throws Throwable {
+        people.put(name, new Person(network, 0));
+    }
+
+    @Given("people are located at")
+    public void people_are_located_at(@Transpose List<Whereabouts> whereabouts) {
+        for (Whereabouts whereabout : whereabouts) {
+            people.put(whereabout.name, new Person(network, whereabout.location));
+        }
+    }
+
+    @When("Sean shouts")
+    public void sean_shouts() throws Throwable {
+        people.get("Sean").shout("Hello, world");
     }
 
     @When("Sean shouts {string}")
-    public void sean_shouts(String message) throws Throwable {
+    public void sean_shouts_message(String message) throws Throwable {
         people.get("Sean").shout(message);
         messageFromSean = message;
     }
 
+    @When("Sean shouts the following message")
+    public void sean_shouts_the_following_message(String message) throws Throwable {
+        people.get("Sean").shout(message);
+        messageFromSean = message;
+        System.out.println(message);
+    }
+
     @Then("Lucy should hear Sean's message")
     public void lucy_hears_Sean_s_message() throws Throwable {
-        assertEquals(asList(messageFromSean), people.get("Lucy").getMessagesHeard());
+        assertEquals(Collections.singletonList(messageFromSean), people.get("Lucy").getMessagesHeard());
+    }
+
+    @Then("Lucy should hear a shout")
+    public void lucy_should_hear_a_shout() throws Throwable {
+        assertEquals(1, people.get("Lucy").getMessagesHeard().size());
+    }
+
+    @Then("{word} should not hear a shout")
+    public void larry_should_not_hear_a_shout(String name) throws Throwable {
+        assertEquals(0, people.get(name).getMessagesHeard().size());
+    }
+
+    @Then("Lucy hears the following messages:")
+    public void lucy_hears_the_following_messages(io.cucumber.datatable.DataTable expectedMessages) {
+        List<List<String>> actualMessages = new ArrayList<List<String>>();
+        List<String> heard = people.get("Lucy").getMessagesHeard();
+        for (String message: heard) {
+            actualMessages.add(Collections.singletonList(message));
+        }
+        expectedMessages.diff(DataTable.create(actualMessages));
     }
 }
